1726b6890963087ee96383776ca4aa0aca722c39
Vo Van Do <dovv1987@gmail.com>
2017-05-31 17:55:00 +0700

    v2_8 update

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,19 +4,33 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use SEO;
+use SEOMeta;
+use OpenGraph;
+use Twitter;
 
 class HomeController extends Controller
 {
-    public function __construct()
-    {
-        // code
-    }
-
     public function index()
     {
-        SEO::setTitle('Gia Sư Trí Tuệ | Trung tâm gia sư dạy kèm uy tín chất lượng')
-            ->setDescription('Trung tâm Gia sư dạy kèm uy tín: Đội ngũ gia sư dạy kèm giỏi nhiều năm giảng dạy (Miễn Phí dạy thử 3b, Cam kết tiến bộ trong 10 buổi) LH: 01674.537.055.');
+        SEOMeta::setTitle('Gia Sư Trí Tuệ | Trung tâm gia sư dạy kèm uy tín chất lượng');
+        SEOMeta::setDescription('Trung tâm Gia sư dạy kèm uy tín: Đội ngũ gia sư dạy kèm giỏi nhiều năm giảng dạy (Miễn Phí dạy thử 3b, Cam kết tiến bộ trong 10 buổi) LH: 01674.537.055.');
+        SEOMeta::setCanonical(request()->getUri());
+        SEOMeta::addMeta('article:published_time', '2017-05-30 12:00:01', 'property');
+        SEOMeta::addMeta('article:section', 'Home', 'property');
+        SEOMeta::addKeyword(['Gia su', 'Tim gia su', 'Gia su gioi', 'Gia su uy tin']);
+
+        OpenGraph::setTitle('Gia Sư Trí Tuệ | Trung tâm gia sư dạy kèm uy tín chất lượng');
+        OpenGraph::setDescription('Trung tâm Gia sư dạy kèm uy tín: Đội ngũ gia sư dạy kèm giỏi nhiều năm giảng dạy (Miễn Phí dạy thử 3b, Cam kết tiến bộ trong 10 buổi) LH: 01674.537.055.');
+        OpenGraph::setUrl(request()->getUri());
+        OpenGraph::addProperty('type', 'articles');
+        OpenGraph::setSiteName('Gia Su Tri Tue');
+        OpenGraph::addImage(request()->getUri().'img/1.png');
+
+        Twitter::setTitle('Gia Sư Trí Tuệ | Trung tâm gia sư dạy kèm uy tín chất lượng');
+        Twitter::setDescription('Trung tâm Gia sư dạy kèm uy tín: Đội ngũ gia sư dạy kèm giỏi nhiều năm giảng dạy (Miễn Phí dạy thử 3b, Cam kết tiến bộ trong 10 buổi) LH: 01674.537.055.');
+        Twitter::setUrl(request()->getUri());
+        Twitter::addImage(request()->getUri().'img/1.png');
+        Twitter::setSite('card');
 
         return view('front.index');
     }
--- config/app.php
+++ config/app.php
@@ -244,6 +244,10 @@ return [
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
         'Image' => Intervention\Image\Facades\Image::class,
+        // SEO
+        'SEOMeta'   => Artesaos\SEOTools\Facades\SEOMeta::class,
+        'OpenGraph' => Artesaos\SEOTools\Facades\OpenGraph::class,
+        'Twitter'   => Artesaos\SEOTools\Facades\TwitterCard::class,
         'SEO' => Artesaos\SEOTools\Facades\SEOTools::class,
     ],
 ];
